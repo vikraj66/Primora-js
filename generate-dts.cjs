739c84baa4078f2773e15ca60128acc9f568b86b
vikraj66 <vikasraj041013@gmail.com>
2024-06-06 11:49:05 +0530

    chore: Update generate-dts.cjs to use require instead of import

--- generate-dts.cjs
+++ generate-dts.cjs
@@ -1,13 +1,7 @@
-import { generateDtsBundle } from 'dts-bundle-generator';
-import { fileURLToPath } from 'url';
-import path from 'path';
-import fs from 'fs';
+const { generateDtsBundle } = require('dts-bundle-generator');
+const path = require('path');
+const fs = require('fs');
 
-// Get the directory name of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Generate the bundle
 const output = generateDtsBundle([
     {
         filePath: path.resolve(__dirname, './src/index.ts'),
@@ -17,6 +11,5 @@ const output = generateDtsBundle([
     },
 ]);
 
-// Write the output to a single file
 const outputPath = path.resolve(__dirname, './dist/index.d.ts');
 fs.writeFileSync(outputPath, output.join('\n'));
