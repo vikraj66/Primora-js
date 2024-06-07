062abb9cf43093794cf88af8faf5740e5fa96e6d
vikraj66 <vikasraj041013@gmail.com>
2024-06-07 13:00:08 +0530

    chore: Update package.json version to 2.0.0 and remove .npmignore file

--- src/core/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Model';
-export * from './Attributes';
-export * from './Collection';
-export * from './ApiSync';
-export * from './Eventing';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export * from './core/models';
-export * from './core/views';
-export * from './router';
-export * from './utils';
+export { Model } from './core/models/Model';
+export { Attributes } from './core/models/Attributes';
+export { Collection } from './core/models/Collection';
+export { ApiSync, Syncable } from './core/models/ApiSync';
+export { Eventing } from './core/models/Eventing';
+export { View } from './core/views/View'
+export { CollectionView } from './core/views/CollectionView';
+export { Router } from './router/Router'
+export { html } from './utils/htm';
+export { applyScopedStyles, loadAndApplyScopedStyles } from './utils/scopedStyles';
